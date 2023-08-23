a01f1ba686550097d1d71f7189a725306b5c4a93
Ashish Mittal <25270853+VampishWolf@users.noreply.github.com>
2023-08-24 00:58:42 +0530

    tranpiling module

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@usecapsule/web-sdk', '@usecapsule/user-management-client'],
 }
 
 module.exports = nextConfig
